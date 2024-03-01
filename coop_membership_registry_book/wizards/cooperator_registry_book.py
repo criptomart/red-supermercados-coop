aa0ffe237acafe2d1c0903bff7ea6ef2c28db395
luisnore <luis@criptomart.net>
2024-03-01 10:57:41 +0100

    LaOsaCoop/laosa#382 Fixes the cooperators book by accumulating data between runs

--- coop_membership_registry_book/wizards/cooperator_registry_book.py
+++ coop_membership_registry_book/wizards/cooperator_registry_book.py
@@ -4,31 +4,37 @@
 from odoo import _, models, api, fields
 import logging
 from collections import defaultdict
+
 _logger = logging.getLogger(__name__)
 
+
 class CooperatorRegistryBook(models.TransientModel):
-    _name = 'cooperator.registry.book'
-    _description = 'Wizard Cooperators Registry Book'
+    _name = "cooperator.registry.book"
+    _description = "Wizard Cooperators Registry Book"
 
     name = fields.Char(string="name")
-    summary = {
-        "total_capital_subscriptions": 0,
-        "total_shares": 0,
-        "cooperators": 0,
-        "old_cooperators": 0,
-        "amount_per_type": defaultdict(int)
-    }
+    summary = {}
 
     def _get_cooperators_data(self, query):
 
-        partners = self.env['res.partner'].search_read(
+        partners = self.env["res.partner"].search_read(
             [query],
             [
-                'name', 'barcode_base', 'amount_subscription', 'fundraising_partner_type_ids',
-                'vat', 'country_id', 'joining_date', 'leaving_date', 'street', 'zip', 'city',
-                'state_id', 'invoice_ids'
+                "name",
+                "barcode_base",
+                "amount_subscription",
+                "fundraising_partner_type_ids",
+                "vat",
+                "country_id",
+                "joining_date",
+                "leaving_date",
+                "street",
+                "zip",
+                "city",
+                "state_id",
+                "invoice_ids",
             ],
-            order="barcode_base"
+            order="barcode_base",
         )
 
         if query[0] == "is_member":
@@ -37,23 +43,31 @@ class CooperatorRegistryBook(models.TransientModel):
             self.summary["old_cooperators"] = len(partners)
 
         for coop in partners:
-            sub_req = self.env['account.invoice'].search_read(
+            sub_req = self.env["account.invoice"].search_read(
                 [
                     ("id", "in", coop["invoice_ids"]),
                     ("state", "not in", ["draft", "cancelled"]),
                     ("is_capital_fundraising", "=", True),
                 ],
                 [
-                    'date_invoice', "invoice_line_ids", 'partner_owned_share_id',
-                    'fundraising_category_id', 'amount_total_signed', 'state'
+                    "date_invoice",
+                    "invoice_line_ids",
+                    "partner_owned_share_id",
+                    "fundraising_category_id",
+                    "amount_total_signed",
+                    "state",
                 ],
-                order="date"
+                order="date",
             )
             for req in sub_req:
-                self.summary["total_capital_subscriptions"] += req["amount_total_signed"]
-                self.summary["amount_per_type"][req['fundraising_category_id'][1]] += req["amount_total_signed"]
+                self.summary["total_capital_subscriptions"] += req[
+                    "amount_total_signed"
+                ]
+                self.summary["amount_per_type"][
+                    req["fundraising_category_id"][1]
+                ] += req["amount_total_signed"]
                 req["quantity"] = 0
-                lines = self.env['account.invoice.line'].browse(req["invoice_line_ids"])
+                lines = self.env["account.invoice.line"].browse(req["invoice_line_ids"])
                 for line in lines:
                     req["quantity"] += line.quantity
                     if req["amount_total_signed"] > 0:
@@ -65,14 +79,20 @@ class CooperatorRegistryBook(models.TransientModel):
         return partners
 
     def print_cooperator_registry_book(self):
-
+        self.summary = {
+            "total_capital_subscriptions": 0,
+            "total_shares": 0,
+            "cooperators": 0,
+            "old_cooperators": 0,
+            "amount_per_type": defaultdict(int),
+        }
         query = ("is_member", "=", True)
         coop_data = self._get_cooperators_data(query)
         query = ("is_former_member", "=", True)
         old_coop_data = self._get_cooperators_data(query)
-        data = {
-            "coops": coop_data,
-            "old_coops": old_coop_data,
-            "summary": self.summary
-        }
-        return self.env.ref('coop_membership_registry_book.action_report_cooperator_registry_book').report_action(None, data=data)
+        data = {"coops": coop_data, "old_coops": old_coop_data, "summary": self.summary}
+        action = self.env.ref(
+            "coop_membership_registry_book.action_report_cooperator_registry_book"
+        ).report_action(None, data=data)
+        action.update({"close_on_report_download": True})
+        return action
--- l10n_es_cooperator_registry_book/wizards/cooperator_registry_book.py
+++ l10n_es_cooperator_registry_book/wizards/cooperator_registry_book.py
@@ -6,29 +6,34 @@ from collections import defaultdict
 
 
 class CooperatorRegistryBook(models.TransientModel):
-    _name = 'cooperator.registry.book'
-    _description = 'Wizard Cooperators Registry Book'
+    _name = "cooperator.registry.book"
+    _description = "Wizard Cooperators Registry Book"
 
     name = fields.Char(string="name")
-    date_start = fields.Date('Fecha Inicio')
-    date_end = fields.Date('Fecha Fin')
-    summary = {
-        "total_capital_subscriptions": 0,
-        "total_shares": 0,
-        "cooperators": 0,
-        "old_cooperators": 0,
-        "amount_per_type": defaultdict(int)
-    }
-
+    date_start = fields.Date("Fecha Inicio")
+    date_end = fields.Date("Fecha Fin")
+    summary = {}
 
     @api.model
     def _get_cooperators_data(self, query):
 
-        partners = self.env['res.partner'].search_read(
+        partners = self.env["res.partner"].search_read(
             [query],
-            ['name', 'cooperator_register_number', 'cooperator_type', 'vat', 'country_id',
-             'effective_date', 'street', 'zip', 'city', 'state_id', 'subscription_request_ids', 'total_value'],
-            order="cooperator_register_number"
+            [
+                "name",
+                "cooperator_register_number",
+                "cooperator_type",
+                "vat",
+                "country_id",
+                "effective_date",
+                "street",
+                "zip",
+                "city",
+                "state_id",
+                "subscription_request_ids",
+                "total_value",
+            ],
+            order="cooperator_register_number",
         )
 
         if query[0] == "member":
@@ -37,29 +42,49 @@ class CooperatorRegistryBook(models.TransientModel):
             self.summary["old_cooperators"] = len(partners)
 
         for coop in partners:
-            sub_req = self.env['subscription.request'].search_read(
-                [("id", "in", coop["subscription_request_ids"]),
-                 ("state", "not in", ["draft", "cancelled"])],
-                ['date', 'ordered_parts', 'share_product_id', 'subscription_amount', 'state', 'state', 'type'],
-                order="date"
+            sub_req = self.env["subscription.request"].search_read(
+                [
+                    ("id", "in", coop["subscription_request_ids"]),
+                    ("state", "not in", ["draft", "cancelled"]),
+                ],
+                [
+                    "date",
+                    "ordered_parts",
+                    "share_product_id",
+                    "subscription_amount",
+                    "state",
+                    "state",
+                    "type",
+                ],
+                order="date",
             )
             for req in sub_req:
-                self.summary["total_capital_subscriptions"] += req["subscription_amount"]
+                self.summary["total_capital_subscriptions"] += req[
+                    "subscription_amount"
+                ]
                 self.summary["total_shares"] += req["ordered_parts"]
-                self.summary["amount_per_type"][req['share_product_id'][1]] += req["subscription_amount"]
+                self.summary["amount_per_type"][req["share_product_id"][1]] += req[
+                    "subscription_amount"
+                ]
             coop["sub_req"] = sub_req
         return partners
 
     def print_cooperator_registry_book(self):
-
+        self.summary = {
+            "total_capital_subscriptions": 0,
+            "total_shares": 0,
+            "cooperators": 0,
+            "old_cooperators": 0,
+            "amount_per_type": defaultdict(int),
+        }
         query = ("member", "=", True)
         coop_data = self._get_cooperators_data(query)
-        query = ('old_member', '=', True)
+        query = ("old_member", "=", True)
         old_coop_data = self._get_cooperators_data(query)
 
-        data = {
-            "coops": coop_data,
-            "old_coops": old_coop_data,
-            "summary": self.summary
-        }
-        return self.env.ref('l10n_es_cooperator_registry_book.action_report_cooperator_registry_book').report_action(None, data=data)
+        data = {"coops": coop_data, "old_coops": old_coop_data, "summary": self.summary}
+        action = self.env.ref(
+            "l10n_es_cooperator_registry_book.action_report_cooperator_registry_book"
+        ).report_action(None, data=data)
+        action.update({"close_on_report_download": True})
+        return action
